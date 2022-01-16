ff9861bd58abb6c89a6c113d671c54c94ed929e9
jokj624 <jokj624@gmail.com>
2022-01-16 22:39:42 +0900

    [FEAT] user router 분기

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,9 @@
 //router index file
 import { Router } from 'express';
-
+import userRouter from './userRouter';
 
 const router = Router();
 
-
+router.use('/user', userRouter);
 
 export default router;
