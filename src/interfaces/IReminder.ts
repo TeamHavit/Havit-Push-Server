faf53f5e16aee1ae137074064d4526119b9f9312
jokj624 <jokj624@gmail.com>
2022-01-17 01:38:31 +0900

    [FIX] 인터페이스 수정

--- src/interfaces/IReminder.ts
+++ src/interfaces/IReminder.ts
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 export interface IReminder {
     userId: mongoose.Schema.Types.ObjectId;
+    contentId: Number;
     ogTitle: String;
     ogImage: String;
     time: Date;
