3d819dcc0e07549351511ead06029be67d1c72e7
jokj624 <jokj624@gmail.com>
2022-01-17 18:38:28 +0900

    [FEAT] 스케줄 db  수정 서비스 로직 작성

--- src/services/scheduleService.ts
+++ src/services/scheduleService.ts
@@ -13,6 +13,18 @@ const createSchedule = async (data: ISchedule) => {
     }
 };
 
+const updateSchedule = async (data: ISchedule) => {
+    const { _id, sendAt } = data;
+    try {
+        const updatedSchedule = await Schedule.findOneAndUpdate({ _id: _id }, { sendAt: sendAt});
+        return updatedSchedule;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+};
+
 export default {
-    createSchedule
+    createSchedule,
+    updateSchedule
 }
